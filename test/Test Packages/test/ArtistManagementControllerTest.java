e0418d6757184049764520c96b598389df1cd80f
2dam <2dam@HZ359575.TartangaLH.eus>
2021-01-08 10:49:11 +0100

    Test package

---
+++ test/Test Packages/test/ArtistManagementControllerTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import javafx.stage.Stage;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.testfx.framework.junit.ApplicationTest;
+
+/**
+ *
+ * @author Lorena
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ArtistManagementControllerTest extends ApplicationTest {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
+
+    @Test
+    public void test1_UserExists() {
+        
+    }
+
+}
