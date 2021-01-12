a4907fa390e5760cf487307d539b2aad74eb0dfe
grupo2i <72964155+grupo2i@users.noreply.github.com>
2021-01-12 08:58:15 +0100

    Update LogInController.java

--- src/reto2desktopclient/view/LogInController.java
+++ src/reto2desktopclient/view/LogInController.java
@@ -5,5 +5,13 @@ package reto2desktopclient.view;
  * @author Aitor Fidalgo
  */
 public class LogInController {
+
+    void setStage(Stage stage) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void initStage(Parent root) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
